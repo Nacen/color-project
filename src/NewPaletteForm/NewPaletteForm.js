514645c96996650739da9413cd95e08494ca8770
Vincent <overlordnacen@gmail.com>
2019-08-25 19:41:29 +0800

    Add Drawer to New PaletteForm

--- src/NewPaletteForm/NewPaletteForm.js
+++ src/NewPaletteForm/NewPaletteForm.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import SideDrawer from '../SideDrawer/SideDrawer';
+
 
 class NewPaletteForm extends Component {
   render() {
     return (
       <div>
-        <h1>New Palette Form</h1>
+        <SideDrawer>
+          <h1>Hello World!</h1>
+        </SideDrawer>
       </div>
     )
   }
