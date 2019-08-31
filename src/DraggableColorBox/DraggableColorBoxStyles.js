3b919b58b5a3d0b092124b8bba1957e82173bbd3
Vincent <overlordnacen@gmail.com>
2019-08-31 18:24:55 +0800

    Add Responsiveness to ColorBox

--- src/DraggableColorBox/DraggableColorBoxStyles.js
+++ src/DraggableColorBox/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import screenSize from '../styles/screenSizes';
+import chroma from 'chroma-js';
 export default {
   root: {
     width: "20%",
@@ -6,23 +8,41 @@ export default {
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
-    marginBottom: "-6.5px"
+    marginBottom: "-6.5px",
+    [screenSize.down("lg")] : {
+      width: "25%",
+      height: "20%"
+    },
+    [screenSize.down("md")] : {
+      width: "50%",
+      height: "10%"
+    },
+    [screenSize.down("sm")] : {
+      width: "100%",
+      height: "5%"
+    }
   },
   boxContent: {
     position: "absolute",
+    left: "0px",
+    bottom: "0px",
     width: "100%",
-    left: "0",
-    bottom: "0",
-    padding: "10px",
-    color: "rgba(0, 0, 0, 0.5)",
+    padding: "5px 10px",
+    color: props =>
+      chroma(props.color).luminance() <= 0.08
+        ? "rgba(255,255,255,0.8)"
+        : "rgba(0,0,0,0.6)",
     letterSpacing: "1px",
     textTransform: "uppercase",
     fontSize: "12px",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    [screenSize.down("sm")] : {
+      padding: "0 10px"
+    }
   },
   deleteIcon: {
-    transition: "all .3s",
+    transition: "all .3s ease-in-out",
     "&:hover": {
       color: "white",
       transform: "scale(1.3)"
