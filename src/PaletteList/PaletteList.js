113ecda99f03fbdd7826cbef6da314947074ba5e
Vincent <overlordnacen@gmail.com>
2019-09-01 18:22:57 +0800

    Animate Palette Deletion

--- src/PaletteList/PaletteList.js
+++ src/PaletteList/PaletteList.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
 import styles from "../styles/PaletteListStyles";
 import MiniPalette from "../MiniPalette/MiniPalette";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class PaletteList extends Component {
   goToPalette = id => {
@@ -15,19 +16,21 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1>React Colors</h1>
+            <h1 className={classes.heading}>React Colors</h1>
             <Link to="/palette/new">Create A Palette</Link>
           </nav>
-          <div className={classes.palettes}>
+          <TransitionGroup className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette
-                handleDelete={() => deletePalette(palette.id)}
-                key={palette.id}
-                {...palette}
-                handleClick={() => this.goToPalette(palette.id)}
-              />
+              <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                <MiniPalette
+                  handleDelete={() => deletePalette(palette.id)}
+                  key={palette.id}
+                  {...palette}
+                  handleClick={() => this.goToPalette(palette.id)}
+                />
+              </CSSTransition>
             ))}
-          </div>
+          </TransitionGroup>
         </div>
       </div>
     );
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,11 +1,30 @@
 import screenSize from "./screenSizes";
+import background from "./background.svg";
 export default {
+  "@global": {
+    ".fade-exit": {
+      opacity: 1
+    },
+    ".fade-exit-active": {
+      opacity: 0,
+      transition: "opacity 500ms ease-out"
+    }
+  },
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: "#394bad",
+    /* background by SVGBackgrounds.com */
+    backgroundImage: `url(${background})`,
+    overflowY: "scroll"
+  },
+  heading: {
+    fontSize: "2rem",
+    [screenSize.down("sm")]: {
+      fontSize: "1.5rem"
+    }
   },
   container: {
     width: "50%",
@@ -37,12 +56,14 @@ export default {
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
     gridGap: "2.5rem",
+    marginBottom: "2.5rem",
     [screenSize.down("md")]: {
       gridTemplateColumns: "repeat(2, 50%)"
     },
     [screenSize.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem"
+      gridGap: "1rem",
+      marginBottom: "1rem"
     }
   }
 };
