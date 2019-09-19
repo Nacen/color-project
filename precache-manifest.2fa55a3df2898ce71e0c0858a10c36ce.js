d478ad634b84f5b07335026aae07deab791ac169
Vincent <overlordnacen@gmail.com>
2019-09-19 10:14:04 +0800

    Updates

---
+++ precache-manifest.2fa55a3df2898ce71e0c0858a10c36ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7108342b8fc535b65aa57cb389d2f865",
+    "url": "/color-project/index.html"
+  },
+  {
+    "revision": "b2abbdaa2c9659946843",
+    "url": "/color-project/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "cad4901740d2d417f1ff",
+    "url": "/color-project/static/css/main.172954de.chunk.css"
+  },
+  {
+    "revision": "b2abbdaa2c9659946843",
+    "url": "/color-project/static/js/2.5f69d78e.chunk.js"
+  },
+  {
+    "revision": "cad4901740d2d417f1ff",
+    "url": "/color-project/static/js/main.1e6a37f3.chunk.js"
+  },
+  {
+    "revision": "baadecf6c4ea25dd41cf",
+    "url": "/color-project/static/js/runtime~main.572f2953.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-project/static/media/background.77428cd4.svg"
+  }
+]);
